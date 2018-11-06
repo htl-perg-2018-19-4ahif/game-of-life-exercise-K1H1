da31495ac5386d9c47327b93db82a9e284cdba77
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-11-06 18:56:51 +0100

    -Tried to generate random cells and draw -made multidimensional arrays -tried to check the neighbor cells -> game of life rules

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 window.onload = () => {
-  const boardSize = 800;
+  const boardSize = 200;
 
+
+  let random: number[][];         //random Array
+  let mirrorRandom: number[][];    //Array where it's noted down if a cell is alive(1) or dead(0)
+  mirrorRandom = random;
+  // let liveCount: number = 0;     //counter of the cells that are alive
+  let totalCells: number;
+
+  //-----------CANVAS----------
   // Get reference to canvas
   const canvas = <HTMLCanvasElement>document.getElementById('canvas');
   canvas.width = canvas.height = boardSize;
@@ -10,12 +18,77 @@ window.onload = () => {
   // Call 'draw' function whenever browser renders a frame on the screen
   window.requestAnimationFrame(draw);
 
+  //---------GAME FUNCTION-------------------
   function draw() {
-    // Demo code showing how to draw in the canvas
-    ctx.clearRect(0, 0, boardSize, boardSize);
-    ctx.fillRect(10, 10, 30, 30);
+    fillRandomly(); //fills multidimensional Array with random numbers
+    drawCells();
+    updateField();
 
-    
     window.requestAnimationFrame(draw);
+  }
+
+
+  //set random cells:
+
+  function fillRandomly() {
+    for (let i = 100; i < 100 - canvas.height; i++) {
+      for (let j = 100; j < 100 - canvas.width; j++) {
+        random[i][j] = Math.floor(Math.random() * boardSize);
+      }
+    }
+
+  }
+
+  //draw cells
+  function drawCells() {
+    ctx.clearRect(0, 0, boardSize, boardSize);
+    for (let i = 0; i < canvas.height; i++) {
+      for (let j = 0; j < canvas.width; j++) {
+        ctx.fillRect(i, j, 4, 4);
+
+      }
+    }
+
+  }
+
+  function updateField() {
+    for (let i = 0; i < canvas.height; i++) {
+      for (let j = 0; j < canvas.width; j++) {
+        totalCells = 0;
+
+        //add the number in the neighbour cells -> alive = number 1 in neighbor cell dead = number 0 in neighbor cell
+        totalCells += random[i - 1][j - 1]; //top left
+        totalCells += random[i - 1][j]; //top center
+        totalCells += random[j - 1][j + 1]; //top right
+
+        totalCells += random[i][j - 1]; //middle left
+        totalCells += random[i][j + 1]; //middle right
+
+        totalCells += random[i + 1][j - 1]; //bottom left
+        totalCells += random[i + 1][j]; //bottom center
+        totalCells += random[i + 1][j + 1]; //bottom right
+
+
+
+        //check "game of life" - rules based on the sum of the neighbor cells
+        switch (totalCells) {
+          case 2: {
+            mirrorRandom[i][j] = random[i][j]; break; //nothing changes
+          }
+
+
+          case 3: {
+            mirrorRandom[i][j] = 1; break;        //alive
+          }
+
+          default: {
+            mirrorRandom[i][j] = 0;                      //dead
+          }
+
+
+        }
+
+      }
+    }
   }
 };
