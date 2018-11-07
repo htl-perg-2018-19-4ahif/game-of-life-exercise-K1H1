45dad7f9cdc91cf4ad843efae1abf6e0273af1c7
Katrin Haslinger <Katrin.Haslinger@gmx.at>
2018-11-07 19:05:06 +0100

    -changed some little things -fixed little bugs -tried to draw cells correctly

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
 window.onload = () => {
   const boardSize = 800;
+  let pixel = 4;
 
 
   let random: number[][];         //random Array
   let mirrorRandom: number[][] = random;    //Array where it's noted down if a cell is alive(1) or dead(0)
   // let liveCount: number = 0;     //counter of the cells that are alive
   let numCells: number;
+  let rows: number = boardSize / pixel;
+  let columns: number = boardSize / pixel;
 
   //-----------CANVAS----------
   // Get reference to canvas
@@ -14,24 +17,29 @@ window.onload = () => {
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
 
+
+
+  //---------GAME---------------
+  fillRandomly(); //fills multidimensional Array with random numbers
+
   // Call 'draw' function whenever browser renders a frame on the screen
   window.requestAnimationFrame(draw);
 
-  //---------GAME FUNCTION-------------------
   function draw() {
-    fillRandomly(); //fills multidimensional Array with random numbers
     drawCells();
     updateField();
 
     window.requestAnimationFrame(draw);
   }
 
+  //---------FUNCTIONS---------------
+
   //draw cells
   function drawCells() {
-    ctx.clearRect(0, 0, canvas.height, canvas.width);
-    for (let i = 0; i < canvas.height; i++) {
-      for (let j = 0; j < canvas.width; j++) {
-        ctx.fillRect(4, 4, i, j);
+    // ctx.clearRect(0, 0, canvas.height, canvas.width);
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        ctx.fillRect(j * 4, i * 4, 4, 4);
 
       }
     }
@@ -39,34 +47,39 @@ window.onload = () => {
 
   //set random cells:
   function fillRandomly() {
-    for (let i = 100; i < 100 - canvas.height; i++) {
-      for (let j = 100; j < 100 - canvas.width; j++) {
-        random[i][j] = Math.floor(Math.random() * boardSize);
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        random[i][j] = Math.floor(Math.random());   //Generates random number -> 0 or 1
       }
     }
 
   }
 
   function updateField() {
+    numCells = 0;
     for (let i = 0; i < canvas.height; i++) {
       for (let j = 0; j < canvas.width; j++) {
 
         //add the number in the neighbour cells -> alive = number 1 in neighbor cell      dead = number 0 in neighbor cell
-        numCells += random[j - 1][j + 1]; //top right
-        numCells += random[i - 1][j - 1]; //top left
-        numCells += random[i - 1][j];     //top center
 
-        numCells += random[i][j + 1]; //middle right
-        numCells += random[i][j - 1]; //middle left
+        //top
+        numCells += random[j - 1][j + 1]; //right
+        numCells += random[i - 1][j - 1]; //left
+        numCells += random[i - 1][j];     //middle
 
-        numCells += random[i + 1][j - 1]; //bottom left
-        numCells += random[i + 1][j + 1]; //bottom right
-        numCells += random[i + 1][j]; //bottom center
+        //middle
+        numCells += random[i][j + 1]; //right
+        numCells += random[i][j - 1]; // left
+
+        //bottom
+        numCells += random[i + 1][j + 1]; //right
+        numCells += random[i + 1][j - 1]; //left
+        numCells += random[i + 1][j];    //middle
 
         //check "game of life" - rules based on the sum of the neighbor-cell-numbers added above 
         switch (numCells) {
           case 2: {
-            mirrorRandom[i][j] = random[i][j]; break; //nothing changes
+            mirrorRandom[i][j] = random[i][j]; break;
           }
 
           case 3: {
